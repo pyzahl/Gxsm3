c6e9e22712868c2751100ea9b7b3872366a38cbd
Percy <22110415+pyzahl@users.noreply.github.com>
2017-04-06 04:43:57 +0000

    reverted devel glsl path to install path

--- gxsm3/vsurf3d.C
+++ gxsm3/vsurf3d.C
@@ -84,7 +84,7 @@ void surf3d_write_schema (){
 	gnome_res_destroy (gl_pref);
 }
 
-#define __GXSM_PY_DEVEL
+//#define __GXSM_PY_DEVEL
 #ifdef __GXSM_PY_DEVEL
 #define GLSL_DEV_DIR "/home/pzahl/SVN/Gxsm-3.0/gl-400/"
 //#define GLSL_DEV_DIR "/home/percy/SVN/Gxsm-3.0/gl-400/"
@@ -473,7 +473,7 @@ public:
                 //glGenTextures(1, &tex); // done globally
                 glBindTexture(GL_TEXTURE_2D, TextureName[2]);
                 tex = TextureName[2];
-                glUniform1i (glGetUniformLocation (Tesselation_ProgramName, "textTexture"), 2);
+                glUniform1i (glGetUniformLocation (S3D_ProgramName, "textTexture"), 2);
 
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
