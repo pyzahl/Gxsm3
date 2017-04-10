94531c7a8b96cbea22fa0c97476596f62966488f
zahl <zahl@384ef51f-bff5-4353-ae35-7baa51b8d721>
2017-04-10 04:21:37 +0000

    added ico

---
+++ trunk/Gxsm-3.0/gl-400/ico-evaluation.glsl
@@ -0,0 +1,23 @@
+/* -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 8 c-style: "K&R" -*- */
+/* Ico-TessEval */
+
+#include "g3d-allshader-uniforms.glsl"
+
+layout(std140, column_major) uniform;
+
+layout(triangles, equal_spacing, cw) in;
+in vec3 tcPosition[];
+out vec3 tePosition;
+out vec3 tePatchDistance;
+uniform mat4 Projection;
+uniform mat4 Modelview;
+
+void main()
+{
+    vec3 p0 = gl_TessCoord.x * tcPosition[0];
+    vec3 p1 = gl_TessCoord.y * tcPosition[1];
+    vec3 p2 = gl_TessCoord.z * tcPosition[2];
+    tePatchDistance = gl_TessCoord;
+    tePosition = normalize(p0 + p1 + p2);
+    gl_Position = Projection * Modelview * vec4(tePosition, 1);
+}
