7e629b64f191770dc50b70c157511fc0c882012c
Percy <22110415+pyzahl@users.noreply.github.com>
2017-04-12 22:28:10 +0000

    ico

--- gl-400/ico-evaluation.glsl
+++ gl-400/ico-evaluation.glsl
@@ -5,7 +5,8 @@
 
 layout(triangles, equal_spacing, cw) in;
 
-uniform vec4 IcoPositionS;
+uniform vec4 IcoPosition;
+uniform vec4 IcoScale;
 
 in block
 {
@@ -19,16 +20,20 @@ out block
         vec3 VertexEye;
 } Out;
 
+float height_transform(float y)
+{
+        return height_scale * y + height_offset;  
+}
 
-//    vPosition = IcoPositionS.xyz + IcoPositionS.w * Position.xyz;
 
 void main()
 {
-    vec3 p0 = gl_TessCoord.x * In[0].tcPosition;
-    vec3 p1 = gl_TessCoord.y * In[1].tcPosition;
-    vec3 p2 = gl_TessCoord.z * In[2].tcPosition;
-    Out.tePatchDistance = gl_TessCoord;
-    Out.tePosition = normalize(p0 + p1 + p2);
-    Out.VertexEye   = (ModelView * vec4(IcoPositionS.xyz + IcoPositionS.w * Out.tePosition, 1)).xyz;  // eye space
-    gl_Position = ModelViewProjection * vec4(IcoPositionS.xyz + IcoPositionS.w * Out.tePosition, 1);
+        vec3 p0 = gl_TessCoord.x * In[0].tcPosition;
+        vec3 p1 = gl_TessCoord.y * In[1].tcPosition;
+        vec3 p2 = gl_TessCoord.z * In[2].tcPosition;
+        Out.tePatchDistance = gl_TessCoord;
+        Out.tePosition =  IcoPosition.xyz + IcoScale.xyz * normalize(p0 + p1 + p2);
+        Out.tePosition.y = height_transform (Out.tePosition.y);
+        Out.VertexEye   = (ModelView * vec4(Out.tePosition, 1)).xyz;  // eye space
+        gl_Position = ModelViewProjection * vec4(Out.tePosition, 1);
 }
