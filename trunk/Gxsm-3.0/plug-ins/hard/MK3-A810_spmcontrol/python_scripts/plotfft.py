4c6dc4ac1ac0a49adf126d9bbb8410fbfc89ed3d
zahl <zahl@384ef51f-bff5-4353-ae35-7baa51b8d721>
2017-10-04 15:12:37 +0000

    added am controller test mode

--- trunk/Gxsm-3.0/plug-ins/hard/MK3-A810_spmcontrol/python_scripts/plotfft.py
+++ trunk/Gxsm-3.0/plug-ins/hard/MK3-A810_spmcontrol/python_scripts/plotfft.py
@@ -8,13 +8,14 @@ y=np.load ("mk3_recorder_ydata.npy")
 t=np.arange (1.*x.size)
 t=t/150.
 
-fftx = abs (np.fft.rfft(x))
-ffty = abs (np.fft.rfft(y))
-freqs = np.fft.rfftfreq (x.size, t[1]-t[0])
+#fftx = abs (np.fft.rfft(x))
+#ffty = abs (np.fft.rfft(y))
+#freqs = np.fft.rfftfreq (x.size, t[1]-t[0])
 
 
 
-plt.semilogy (freqs, fftx, freqs, ffty)
-plt.title ('FFT')
+#plt.semilogy (freqs, fftx, freqs, ffty)
+plt.plot (t, y)
+plt.title ('FFdT')
 plt.grid (True)
 plt.show ()
