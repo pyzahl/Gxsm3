17a2c6adecc54eb1d97538ba110d0c4109a06293
zahl <zahl@384ef51f-bff5-4353-ae35-7baa51b8d721>
2018-09-19 17:51:33 +0000

    added algorithm simulation tool

--- trunk/Gxsm-3.0/plug-ins/control/RedPitaya-Support/paclocsim.py
+++ trunk/Gxsm-3.0/plug-ins/control/RedPitaya-Support/paclocsim.py
@@ -7,6 +7,44 @@ from random import randint
 
 Q22 = (1<<22)-1
 
+
+class lockin():
+    def __init__(self, f0=1000., frq=125e6, tau=2e-6, iir_f=0.):
+        self.a=0L
+        self.b=0L
+        self.time=0.0
+        self.dt=1.0/frq
+        self.fref=f0
+        self.tau_lp=int(Q22*self.dt/tau)
+        self.iirf = iir_f
+        self.ss = 0L
+        self.cc = 0L
+        
+    def run_step(self, signal):
+        wt=self.time*math.pi*2.*self.fref
+        self.phasedetect (int(signal), int(Q22*math.sin (wt)), int(Q22*math.cos (wt)))
+        self.time = self.time+self.dt
+        return self.time
+
+    def filter (self, old, x):
+        q = Q22 - self.tau_lp
+        return (old * q + x * self.tau_lp + 0x200000)>>22
+    
+# s,c signal 28bit in S5Q22
+    def phasedetect (self, signal, s, c):
+        x = (signal*s + 0x200000) >> 22
+        y = (signal*c + 0x200000) >> 22
+        self.a = self.filter(self.a, x)
+        self.b = self.filter(self.b, y)
+        
+    def ampl (self):
+        return math.sqrt (self.a*self.a + self.b*self.b)/Q22
+
+    def phase (self):
+        return math.atan2 (self.a-self.b, self.a+self.b)
+
+
+
 class lms():
     def __init__(self, f0=1000., frq=125e6, tau=2e-6, iir_f=0.):
         self.a=0L
@@ -31,8 +69,8 @@ class lms():
 
         #// Apply loopback filter on ref
 
-        self.ss=int ((1.-self.iirf)*self.ss+self.iirf*s)
-        self.cc=int ((1.-self.iirf)*self.cc+self.iirf*c)
+        #self.ss=int ((1.-self.iirf)*self.ss+self.iirf*s)
+        #self.cc=int ((1.-self.iirf)*self.cc+self.iirf*c)
 
         ci1t=s-self.ss  # DelayForRef (delayRef, s, &InputRef1[0]);
         cr1t=c-self.cc  # DelayForRef (delayRef, c, &InputRef2[0]);
@@ -67,6 +105,9 @@ class lms():
     def phase (self):
         return math.atan2 (self.a-self.b, self.a+self.b)
 
+
+
+    
 fref=2e6
 xx=1
 xx2=1.8
@@ -74,10 +115,14 @@ xx3=2.4
 ftest=fref*xx
 ftest2=fref*xx2
 ftest3=fref*xx3
-tau=0.15
-na=0.01
+tau=0.15 #0.15 # 0.15
+a1=0.2
+a2=0.0
+a3=0.0
+na=0.0
 tscale=1
 pac = lms(fref, 125e6, tau*1e-6)
+lck = lockin(fref, 125e6, tau*1e-6)
 
 iir    = 5e-4;
 
@@ -89,12 +134,17 @@ m=np.arange (N).astype(np.float)
 sdc=np.arange (N).astype(np.float)
 a=np.arange (N).astype(np.float)
 p=np.arange (N).astype(np.float)
+la=np.arange (N).astype(np.float)
+lp=np.arange (N).astype(np.float)
 
 for i in range (0,N):
+    #if i == 500:
+    #    pac.dt = pac.dt*1.1
+    #    lck.dt = lck.dt*1.1
     wt=i*pac.dt*math.pi*2.*ftest
     wt2=i*pac.dt*math.pi*2.*ftest2
     wt3=i*pac.dt*math.pi*2.*ftest3
-    s[i]=0.2*math.sin(wt)+0.5*math.sin(wt2) #+0.3*math.sin(wt3)+na*randint(-1000, 1000)*1e-3 #+0.2*math.sin(wt/200)
+    s[i]=a1*math.sin(wt) + a2*math.sin(wt2) + a3*math.sin(wt3) + na*randint(-1000, 1000)*1e-3 #+0.2*math.sin(wt/200)
     sdc[i] = s[i]
     if i>1:
         sdc[i]=(1.-iir)*sdc[i-1]+iir*sdc[i]
@@ -103,14 +153,20 @@ for i in range (0,N):
     ts[i]=1e3*pac.run_step(m[i]*Q22)*tscale
     a[i]=pac.ampl()
     p[i]=pac.phase()
+    lck.run_step(m[i]*Q22)*tscale
+    la[i]=lck.ampl()
+    lp[i]=lck.phase()
 
 plt.xlabel('time in ms')
 plt.plot (ts, s, label="signal")
-plt.plot (ts, sdc, label="signal dc")
-plt.plot (ts, m, label="signal-dc")
-plt.plot (ts, a, label="ampl")
-plt.plot (ts, p, label="phase")
-plt.title ('PAC LMS Simulation tau: %g'%tau+'us f=%g'%fref+'Hz fs=x%g'%xx+'+x%g'%xx2+'+x%g'%xx3)
+#plt.plot (ts, sdc, label="signal dc")
+#plt.plot (ts, m, label="signal-dc")
+plt.plot (ts, a, label="LMS-ampl")
+plt.plot (ts, p, label="LMS-phase")
+plt.plot (ts, la, label="LCK-ampl")
+plt.plot (ts, lp, label="LCK-phase")
+#plt.title ('PAConvergence LMS vs. LockIn Simulation tau: %g'%tau+'us f=%g'%fref+'Hz fs=x%g'%xx+'+x%g'%xx2+'+x%g'%xx3)
+plt.title ('PAConvergence LMS vs. LockIn Simulation tau: %g'%tau+'us f=%g'%fref+'Hz')
 plt.grid (True)
 plt.legend()
 plt.show ()
