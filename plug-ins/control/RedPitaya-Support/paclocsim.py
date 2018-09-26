51bd85a53a6ef3b182df9c7491d064a9348d7b0c
Percy <22110415+pyzahl@users.noreply.github.com>
2018-09-26 04:07:57 +0000

    simulation tool work

--- plug-ins/control/RedPitaya-Support/paclocsim.py
+++ plug-ins/control/RedPitaya-Support/paclocsim.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import collections
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -7,9 +7,8 @@ from random import randint
 
 Q22 = (1<<22)-1
 
-
 class lockin():
-    def __init__(self, f0=1000., frq=125e6, tau=2e-6, iir_f=0.):
+    def __init__(self, f0=1000., frq=125e6, tau=2e-6, iir_f=0., navg=1):
         self.a=0L
         self.b=0L
         self.time=0.0
@@ -19,29 +18,57 @@ class lockin():
         self.iirf = iir_f
         self.ss = 0L
         self.cc = 0L
+        self.n = navg*frq/f0
+        self.corrx=np.arange (self.n).astype(np.float)
+        self.corry=np.arange (self.n).astype(np.float)
+        self.corri=0
+        self.sumcorrx=0.0
+        self.sumcorry=0.0
         
     def run_step(self, signal):
         wt=self.time*math.pi*2.*self.fref
-        self.phasedetect (int(signal), int(Q22*math.sin (wt)), int(Q22*math.cos (wt)))
+        self.phasedetect (int(Q22*signal), int(Q22*math.sin (wt)), int(Q22*math.cos (wt)))
         self.time = self.time+self.dt
         return self.time
 
     def filter (self, old, x):
         q = Q22 - self.tau_lp
-        return (old * q + x * self.tau_lp + 0x200000)>>22
+        return int(old * q + x * self.tau_lp + 0x200000)>>22
     
 # s,c signal 28bit in S5Q22
     def phasedetect (self, signal, s, c):
         x = (signal*s + 0x200000) >> 22
         y = (signal*c + 0x200000) >> 22
-        self.a = self.filter(self.a, x)
-        self.b = self.filter(self.b, y)
+        self.sumcorrx = self.sumcorrx + x - self.corrx[self.corri]
+        self.sumcorry = self.sumcorry + y - self.corry[self.corri]
+        self.corrx[self.corri] = x
+        self.corry[self.corri] = y
+        self.corri = self.corri+1
+        if self.corri >= self.n:
+            self.corri=0
         
+        #self.a = self.filter(self.a, x)
+        #self.b = self.filter(self.b, y)
+        self.a = self.filter(self.a, self.sumcorrx)
+        self.b = self.filter(self.b, self.sumcorry)
+        
+    def x (self):
+        return self.a/Q22
+
+    def y (self):
+        return self.b/Q22
+
     def ampl (self):
-        return math.sqrt (self.a*self.a + self.b*self.b)/Q22
+        return 2.0*math.sqrt (self.a*self.a + self.b*self.b)/Q22/self.n
 
     def phase (self):
         return math.atan2 (self.a-self.b, self.a+self.b)
+
+    def campl (self):
+        return 2.0*math.sqrt (self.sumcorrx*self.sumcorrx + self.sumcorry*self.sumcorry)/Q22/self.n
+
+    def cphase (self):
+        return math.atan2 (self.sumcorrx-self.sumcorry, self.sumcorrx+self.sumcorry)
 
 
 
@@ -56,13 +83,17 @@ class lms():
         self.iirf = iir_f
         self.ss = 0L
         self.cc = 0L
+        self.errordet=0.0
+        self.predict=0.0
+        self.delayc = collections.deque(maxlen=5)
+        self.delays = collections.deque(maxlen=5)
         
     def run_step(self, signal):
         wt=self.time*math.pi*2.*self.fref
-        self.phasedetect (int(signal), int(Q22*math.sin (wt)), int(Q22*math.cos (wt)))
+        self.phasedetect (int(Q22*signal), int(Q22*math.sin (wt)), int(Q22*math.cos (wt)))
         self.time = self.time+self.dt
         return self.time
-        
+    
 # s,c signal 28bit in S5Q22
     def phasedetect (self, signal, s, c):
         #// Sin/Cos in Q27 coeff in Q27
@@ -72,19 +103,22 @@ class lms():
         #self.ss=int ((1.-self.iirf)*self.ss+self.iirf*s)
         #self.cc=int ((1.-self.iirf)*self.cc+self.iirf*c)
 
-        ci1t=s-self.ss  # DelayForRef (delayRef, s, &InputRef1[0]);
-        cr1t=c-self.cc  # DelayForRef (delayRef, c, &InputRef2[0]);
+        self.delayc.append(c)
+        self.delays.append(s)
+
+        ci1t = s #self.delays[0] #-self.ss  # DelayForRef (delayRef, s, &InputRef1[0]);
+        cr1t = c #self.delayc[0] #-self.cc  # DelayForRef (delayRef, c, &InputRef2[0]);
 
         #// Compute the prediction
 
         temll = ci1t * self.a  #// Q22 * Q22 : Q44
         temll = temll + cr1t * self.b + 0x200000  #// Q22 * Q22 : Q44
-        predict = temll >> 22 #// Q22
+        self.predict = temll >> 22 #// Q22
 
         #// Compute d_mu_e  
 
-        errordet = signal-predict #// Q22 - Q22 : Q22
-        temll = errordet * self.tau_pac + 0x200000  #// Q22 * Q22 : Q44 
+        self.errordet = signal- self.predict #// Q22 - Q22 : Q22
+        temll =  self.errordet * self.tau_pac + 0x200000  #// Q22 * Q22 : Q44 
         temll = temll >> 22  #// Q22
         d_mu_e = temll;
 
@@ -98,9 +132,58 @@ class lms():
         temll = temll >> 22  #// Q22
         self.a = self.a+temll
 
+    def lerror (self):
+        return float(self.errordet)/Q22
+
+    def lpredict (self):
+        return float(self.predict)/Q22
 
     def ampl (self):
         return math.sqrt (self.a*self.a + self.b*self.b)/Q22
+
+    def phase (self):
+        return math.atan2 (self.a-self.b, self.a+self.b)
+
+class lms_f():
+    def __init__(self, f0=1000., frq=125e6, tau=2e-6, iir_f=0.):
+        self.a=0.0
+        self.b=0.0
+        self.time=0.0
+        self.dt=1.0/frq
+        self.fref=f0
+        self.tau_pac=self.dt/tau
+        self.iirf = iir_f
+        self.errordet=0.0
+        self.predict=0.0
+        
+    def run_step(self, signal):
+        wt=self.time*math.pi*2.*self.fref
+        self.phasedetect (signal, math.sin (wt), math.cos (wt))
+        self.time = self.time+self.dt
+        return self.time
+        
+    def phasedetect (self, signal, s, c):
+        # Compute the prediction
+        self.predict = s * self.a  +  c * self.b
+
+        # Compute d_mu_e  
+
+        self.errordet = signal- self.predict
+        d_mu_e = self.errordet * self.tau_pac
+
+        # Compute LMS
+
+        self.b = self.b + c * d_mu_e
+        self.a = self.a + s * d_mu_e
+
+    def lerror (self):
+        return self.errordet
+
+    def lpredict (self):
+        return self.predict
+
+    def ampl (self):
+        return math.sqrt (self.a*self.a + self.b*self.b)
 
     def phase (self):
         return math.atan2 (self.a-self.b, self.a+self.b)
@@ -115,18 +198,20 @@ xx3=2.4
 ftest=fref*xx
 ftest2=fref*xx2
 ftest3=fref*xx3
-tau=0.15 #0.15 # 0.15
+tau=0.4 #0.15 # 0.15
 a1=0.2
 a2=0.0
 a3=0.0
-na=0.0
+na=0.2
 tscale=1
-pac = lms(fref, 125e6, tau*1e-6)
-lck = lockin(fref, 125e6, tau*1e-6)
+pac = lms(fref, 124e6, tau*1e-6)
+lck = lockin(fref, 124e6, tau*1e-6)
 
 iir    = 5e-4;
 
-N=1000*tscale
+N=250*tscale
+M=2000
+
 t=np.arange (N).astype(np.float)
 ts=np.arange (N).astype(np.float)
 s=np.arange (N).astype(np.float)
@@ -136,35 +221,76 @@ a=np.arange (N).astype(np.float)
 p=np.arange (N).astype(np.float)
 la=np.arange (N).astype(np.float)
 lp=np.arange (N).astype(np.float)
+x=np.arange (N).astype(np.float)
+y=np.arange (N).astype(np.float)
+lmse=np.arange (N).astype(np.float)
+lmsp=np.arange (N).astype(np.float)
 
-for i in range (0,N):
-    #if i == 500:
-    #    pac.dt = pac.dt*1.1
-    #    lck.dt = lck.dt*1.1
-    wt=i*pac.dt*math.pi*2.*ftest
-    wt2=i*pac.dt*math.pi*2.*ftest2
-    wt3=i*pac.dt*math.pi*2.*ftest3
-    s[i]=a1*math.sin(wt) + a2*math.sin(wt2) + a3*math.sin(wt3) + na*randint(-1000, 1000)*1e-3 #+0.2*math.sin(wt/200)
-    sdc[i] = s[i]
-    if i>1:
-        sdc[i]=(1.-iir)*sdc[i-1]+iir*sdc[i]
-    m[i] = s[i] - sdc[i]
-    t[i]=i
-    ts[i]=1e3*pac.run_step(m[i]*Q22)*tscale
-    a[i]=pac.ampl()
-    p[i]=pac.phase()
-    lck.run_step(m[i]*Q22)*tscale
-    la[i]=lck.ampl()
-    lp[i]=lck.phase()
+
+ar=np.arange (M).astype(np.float)
+lar=np.arange (M).astype(np.float)
+f=np.arange (M).astype(np.float)
+
+for k in range (0,M):
+    ff = float(k)/M
+    ff = pow(10., ff*2.5-2)
+    ftest = ff*fref
+    f[k]=ff
+    if k == M-1:
+        ftest=fref*0.99
+
+    for i in range (0,N):
+        #if i == 500:
+        #    pac.dt = pac.dt*1.1
+        #    lck.dt = lck.dt*1.1
+        wt=i*pac.dt*math.pi*2.*ftest
+        wt2=i*pac.dt*math.pi*2.*ftest2
+        wt3=i*pac.dt*math.pi*2.*ftest3
+        s[i]=a1*math.sin(wt) + a2*math.sin(wt2) + a3*math.sin(wt3) + na*randint(-1000, 1000)*1e-3 #+0.2*math.sin(wt/200)
+        sdc[i] = s[i]
+        if i>1:
+            sdc[i]=(1.-iir)*sdc[i-1]+iir*sdc[i]
+        m[i] = s[i] #- sdc[i]
+        t[i]=i
+        ts[i]=1e3*pac.run_step(m[i])*tscale
+        a[i]=pac.ampl()
+        p[i]=pac.phase()
+        lmse[i]=pac.lerror()
+        lmsp[i]=pac.lpredict()
+        lck.run_step(m[i])*tscale
+        la[i]=lck.ampl()
+        lp[i]=lck.phase()
+        x[i]=lck.x()
+        y[i]=lck.y()
+
+    ar[k] = a[N-1]
+    lar[k] = la[N-1]
+ 
+a[0]=0
+p[0]=0
+la[0]=0
+lp[0]=0
+a[1]=0
+p[1]=0
+la[1]=0
+lp[1]=0
+a[2]=0
+p[2]=0
+la[2]=0
+lp[2]=0
 
 plt.xlabel('time in ms')
 plt.plot (ts, s, label="signal")
 #plt.plot (ts, sdc, label="signal dc")
 #plt.plot (ts, m, label="signal-dc")
+plt.plot (ts, lmsp, label="LMS-predict")
+#plt.plot (ts, lmse, label="LMS-error")
 plt.plot (ts, a, label="LMS-ampl")
 plt.plot (ts, p, label="LMS-phase")
 plt.plot (ts, la, label="LCK-ampl")
 plt.plot (ts, lp, label="LCK-phase")
+#plt.plot (ts, x, label="LCK-X")
+#plt.plot (ts, y, label="LCK-Y")
 #plt.title ('PAConvergence LMS vs. LockIn Simulation tau: %g'%tau+'us f=%g'%fref+'Hz fs=x%g'%xx+'+x%g'%xx2+'+x%g'%xx3)
 plt.title ('PAConvergence LMS vs. LockIn Simulation tau: %g'%tau+'us f=%g'%fref+'Hz')
 plt.grid (True)
@@ -179,3 +305,12 @@ plt.show ()
 #plt.grid (True)
 #plt.legend()
 #plt.show ()
+
+plt.xlabel('Freq in Hz x F-ref')
+plt.plot (f, ar, label="LMS")
+plt.plot (f, lar, label="LockIn")
+#plt.plot (t, p, label="phase")
+#plt.title ('PAC LMS Simulation')
+plt.grid (True)
+plt.legend()
+plt.show ()
