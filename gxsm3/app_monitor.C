bcab87e0ff25443d315a28f4e76e5ffa837c4d2e
Percy <22110415+pyzahl@users.noreply.github.com>
2020-08-17 19:55:15 +0000

    reverted or put update for obsoleted code into #if #else #endif as not widly available yet.

--- gxsm3/app_monitor.C
+++ gxsm3/app_monitor.C
@@ -249,16 +249,19 @@ void MonitorControl::LogEvent (const gchar *Action, const gchar *Entry, gint lev
                 gint lines = gtk_text_buffer_get_line_count (log_buf);
          
                 // append to log buffer view
-                //GTimeVal gt;
-                //g_get_current_time (&gt);
-                //gchar *tmp = g_time_val_to_iso8601 (&gt);
+#if 1 // obsoleted code:
+	        GTimeVal gt;
+                g_get_current_time (&gt);
+                gchar *tmp = g_time_val_to_iso8601 (&gt);
+#else // new -- but not yet widly available:
                 GTimeZone *tz = g_time_zone_new_local ();
                 GDateTime *gdt = g_date_time_new_now (tz);
                 gchar *tmp = g_date_time_format_iso8601 (gdt);
-                GString *output = g_string_new (tmp);
-                g_free (tmp);
                 g_time_zone_unref (tz);
                 g_date_time_unref (gdt);
+#endif
+                GString *output = g_string_new (tmp);
+                g_free (tmp);
                 output = g_string_append(output, ": \t");
                 output = g_string_append(output, Action);
                 output = g_string_append(output, ": \t");
