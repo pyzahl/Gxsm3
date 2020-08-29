c9673b0a5303f067bf92f574444489c3a92aeb9e
stm <stm@384ef51f-bff5-4353-ae35-7baa51b8d721>
2020-08-29 09:03:25 +0000

    * further changes to update build process and make it compatible with snap

--- trunk/Gxsm-3.0/autogen.sh
+++ trunk/Gxsm-3.0/autogen.sh
@@ -27,29 +27,38 @@ test -n "$srcdir" || srcdir=.
 OLDDIR=`pwd`
 cd $srcdir
 
-AUTORECONF=`which autoreconf`
-if test -z $AUTORECONF; then
-        echo "*** No autoreconf found, please intall it ***"
+(test -f configure.ac) || {
+        echo "*** ERROR: Directory '$srcdir' does not look like the top-level project directory ***"
         exit 1
+}
+
+# shellcheck disable=SC2016
+PKG_NAME=$(autoconf --trace 'AC_INIT:$1' configure.ac)
+
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+        echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
+        echo "*** If you wish to pass any to it, please specify them on the" >&2
+        echo "*** '$0' command line." >&2
+        echo "" >&2
 fi
 
-INTLTOOLIZE=`which intltoolize`
-if test -z $INTLTOOLIZE; then
-        echo "*** No intltoolize found, please install the intltool package ***"
-        exit 1
-fi
+aclocal --install || exit 1
+# gtkdocize --copy || exit 1
+intltoolize --force --copy --automake || exit 1
+# glib-gettextize --force --copy || exit 1
+autoreconf --verbose --force --install || exit 1
 
-GNOMEDOC=`which yelp-build`
-if test -z $GNOMEDOC; then
-        echo "*** The tools to build the documentation are not found,"
-        echo "    please intall the yelp-tools package ***"
-        exit 1
-fi
+cd "$olddir"
+if [ "$NOCONFIGURE" = "" ]; then
+        $srcdir/configure "$@" || exit 1
 
-GTKDOCIZE=`which gtkdocize`
-if test -z $GTKDOCIZE; then
-        echo "*** No GTK-Doc found, please install it ***"
-        exit 1
+        if [ "$1" = "--help" ]; then
+                exit 0
+        else
+                echo "Now type 'make' to compile $PKG_NAME" || exit 1
+        fi
+else
+        echo "Skipping configure process."
 fi
 
 #
@@ -68,7 +77,7 @@ done
 
 # end of creation of man-page symlinks.
 
-gnome-autogen.sh $@
+# gnome-autogen.sh $@
 
 echo
 echo "Now type 'make' to compile the $PROJECT."
