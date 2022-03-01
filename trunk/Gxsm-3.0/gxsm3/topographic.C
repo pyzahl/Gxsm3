8d16d848d3e9d32893c9af990f514eb9d198ef47
zahl <zahl@384ef51f-bff5-4353-ae35-7baa51b8d721>
2022-03-01 03:05:34 +0000

    few patches for pyremote related calls

--- trunk/Gxsm-3.0/gxsm3/topographic.C
+++ trunk/Gxsm-3.0/gxsm3/topographic.C
@@ -41,7 +41,7 @@
 #define QSIZE1  QSIZE // Anfang
 #define QSIZE2  (QSIZE-2) // Ende
 
-TopoGraphicScan::TopoGraphicScan(int vtype, int vflg, int ChNo, SCAN_DATA *vd):Scan(vtype, vflg, ChNo, vd){
+TopoGraphicScan::TopoGraphicScan(int vtype, int vflg, int ChNo, SCAN_DATA *vd, ZD_TYPE mtyp):Scan(vtype, vflg, ChNo, vd, mtyp){
 }
 
 TopoGraphicScan::~TopoGraphicScan(){
