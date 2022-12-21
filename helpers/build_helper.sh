3bd2ed81192aaed12d214c0736ba44088eae2917
Stefan Schroeder <ondekoza@gmail.com>
2022-12-21 23:31:31 +0100

    add more options

--- helpers/build_helper.sh
+++ helpers/build_helper.sh
@@ -17,29 +17,50 @@ source /etc/os-release
 
 arg=$1
 
+echo "This is a $ID $VERSION_ID system."
+
 if [[ $arg == "prep" ]] ; then
 
-	if [[ $ID == "debian" ]] ; then
-	   if [[ $VERSION_ID == "11" ]] ; then
-             echo "$0: This is a $ID $VERSION_ID system."
-
-	     sudo apt install autotools-dev debhelper fonts-freefont-ttf \
-	     freeglut3-dev gnome-common gsettings-desktop-schemas-dev \
-	     gtk-doc-tools intltool libfftw3-dev libgail-3-dev \
-	     libgl1-mesa-dev libglew-dev libglm-dev libgsl-dev \
-	     libgtk-3-0 libgtksourceview-3.0-dev \
-	     libgtksourceviewmm-3.0-dev \
-	     libnetcdf-cxx-legacy-dev libnetcdf-dev \
-	     libnlopt-dev libopencv-core-dev libopencv-features2d-dev \
-	     libopencv-highgui-dev libopencv-objdetect-dev libquicktime-dev \
-	     libsoup2.4-dev subversion yelp-tools libpython3-dev \
-             libvala-0.48-dev libenchant-2-dev python3-dev python3-numpy \
-             libopencv-video-dev libopencv-calib3d-dev libpopt-dev
-	   fi
-	else
-	   echo "This is not a supported distro. Please file an issue to support '$ID'"
-	fi
-
+    if [[ $ID == "debian" && $VERSION_ID == "11" ]] ; then
+	sudo apt install -y autotools-dev debhelper fonts-freefont-ttf \
+	freeglut3-dev gnome-common gsettings-desktop-schemas-dev \
+	gtk-doc-tools intltool libfftw3-dev libgail-3-dev \
+	libgl1-mesa-dev libglew-dev libglm-dev libgsl-dev \
+	libgtk-3-0 libgtksourceview-3.0-dev \
+	libgtksourceviewmm-3.0-dev \
+	libnetcdf-cxx-legacy-dev libnetcdf-dev \
+	libnlopt-dev libopencv-core-dev libopencv-features2d-dev \
+	libopencv-highgui-dev libopencv-objdetect-dev libquicktime-dev \
+	libsoup2.4-dev subversion yelp-tools libpython3-dev \
+        libvala-0.48-dev libenchant-2-dev python3-dev python3-numpy \
+        libopencv-video-dev libopencv-calib3d-dev libpopt-dev
+    elif [[ $ID == "ubuntu" && $VERSION_ID == "22.04" ]] ; then
+	sudo apt install -y autotools-dev debhelper fonts-freefont-ttf \
+	freeglut3-dev gnome-common gsettings-desktop-schemas-dev \
+	gtk-doc-tools intltool libfftw3-dev libgail-3-dev \
+	libgl1-mesa-dev libglew-dev libglm-dev libgsl-dev \
+	libgtk-3-0 libgtksourceview-3.0-dev \
+	libgtksourceviewmm-3.0-dev \
+	libnetcdf-cxx-legacy-dev libnetcdf-dev \
+	libnlopt-dev libopencv-core-dev libopencv-features2d-dev \
+	libopencv-highgui-dev libopencv-objdetect-dev libquicktime-dev \
+	libsoup2.4-dev subversion yelp-tools libpython3-dev \
+        libvala-0.56-dev libenchant-2-dev python3-dev python3-numpy \
+        libopencv-video-dev libopencv-calib3d-dev libpopt-dev
+    else
+        echo "This is not a supported distro. "
+        echo "Please file an issue to support '$ID $VERSION_ID'"
+        exit
+    fi
+    echo "Now you can use the 'build' argument."
+elif [[ $arg == "build" ]] ; then
+    ./autogen.sh
+    ./configure
+    make
+elif [[ $arg == "install" ]] ; then
+    sudo make install
+elif [[ $arg == "package" ]] ; then
+    dpkg-buildpackage -us -uc
 else 
     echo "Missing command. Use 'prep'."
 fi
