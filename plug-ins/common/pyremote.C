c42dc61633b6903e9006b89d568f01ff83d16d06
Percy Zahl <pyzahl@gmail.com>
2025-09-05 21:04:08 -0400

    backported OSD position patch

--- plug-ins/common/pyremote.C
+++ plug-ins/common/pyremote.C
@@ -1510,6 +1510,9 @@ to the community. The GXSM-Forums always welcome input.
 
 #include <Python.h>
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+//#include "/usr/local/lib/python3.9/dist-packages/numpy/core/include/numpy/arrayobject.h"
+//#include "/usr/local/lib/python3.9/dist-packages/numpy/core/include/numpy/ndarraytypes.h"
+//#include "/usr/local/lib/python3.9/dist-packages/numpy/core/include/numpy/ndarrayobject.h"
 #include "numpy/arrayobject.h"
 #include "numpy/ndarraytypes.h"
 #include "numpy/ndarrayobject.h"
